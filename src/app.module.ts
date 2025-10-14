fc7f4736a9a0f9bae3c649b50292c0fe865be972
Eric Stanley <eric.stanleyjones@gmail.com>
2025-10-14 23:24:49 +0530

    added download functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TripModule } from './trip/trip.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [TripModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'src', 'assets'),
+      serveRoot: '/static', // optional prefix
+    }),
+    TripModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    // origin: 'http://localhost:4321', // 👈 or your frontend port (e.g., 5173 for Vite)
+    origin: '*', // allows all origins
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
