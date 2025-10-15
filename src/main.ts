37a8af3e97f80f982b98d0768b8fa2b61d5d9559
Eric Stanley <eric.stanleyjones@gmail.com>
2025-10-15 14:51:31 +0530

    fixed cors error for graphql

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,14 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*', // ✅ Allow all origins (or specify your frontend domain)
+      credentials: true,
+    },
+  });
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
-  app.enableCors({
-    // origin: 'http://localhost:4321', // 👈 or your frontend port (e.g., 5173 for Vite)
-    origin: '*', // allows all origins
-    methods: ['GET', 'POST'],
-    credentials: true,
-  });
   await app.listen(3000);
 }
 bootstrap();
