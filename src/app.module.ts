0cd903a3a09062f1373180c139451c1da35b9e1d
Eric Stanley <eric.stanleyjones@gmail.com>
2025-10-16 10:03:30 +0530

    fixed error message response and removed stack trace

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { RequestLoggerMiddleware } from './common/middlewares/request-logger.middleware';
+import { StripNullDataPlugin } from './common/interceptors/error.interceptor';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
@@ -22,6 +23,24 @@ import { ConfigModule } from '@nestjs/config';
       sortSchema: true,
       path: '/graphql',
       playground: true,
+      formatError: (error) => {
+        console.log(error);
+        const cleanedMessage = error.message
+          .replace(/\\\"/g, '"')   // unescape inner quotes
+          .replace(/\"([^\"]+)\"/g, '$1') // remove outer quotes around values
+          .replace(/\\\$/g, '$')   // unescape $ sign
+          .replace(/\$/g, '$')    // ensure $ is preserved
+          .replace(/days days$/, 'days');
+
+        const trimmedMessage = cleanedMessage.includes(';')
+          ? cleanedMessage.split(';')[1].trim()
+          : cleanedMessage.trim();
+
+        return {
+          message: trimmedMessage,
+        };
+      },
+      plugins: [StripNullDataPlugin],
     }),
     TripModule,
     DatabaseModule
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import express from 'express';
 import cors from 'cors';
 
@@ -25,6 +26,19 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new LoggingInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((err) =>
+          Object.values(err.constraints || {}).join(', ')
+        );
+        return new BadRequestException(messages.join('; '));
+      },
+    })
+  );
 
   if (process.env.NODE_ENV !== 'production') {
     await app.listen(3000);
